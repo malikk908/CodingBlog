eac77418daa4c4b1d86c6b3142a1f2a38407c1b1
malikk908 <136706872+malikk908@users.noreply.github.com>
2023-08-08 14:24:31 +0500

    feat - Contact/API

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
+import Script from 'next/script'
+
 
 export default function App({ Component, pageProps }) {
   return <>
   <Navbar/>
+
+
+
   <Component {...pageProps} />
   </>
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Head from 'next/head'
+
+import Script from 'next/script'
 
 const contact = () => {
+
+
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [desc, setDesc] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(name, email, phone, desc)
+    const data = { name, email, phone, desc };
+
+    async function postJSON(data) {
+      try {
+        const response = await fetch("http://localhost:3000/api/postcontact", {
+          method: "POST", // or 'PUT'
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+
+        const result = await response.text();
+        console.log("Success:", result);
+        
+        setName('')
+        setEmail('')
+        setPhone('')
+        setDesc('')
+        alert("Thanks for contacting us")
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+
+    postJSON(data);
+  }
+
+  const handleChange = (e) => {
+    if (e.target.name == 'name') {
+      setName(e.target.value)
+    }
+    else if (e.target.name == 'email') {
+      setEmail(e.target.value)
+    }
+    else if (e.target.name == 'phone') {
+      setPhone(e.target.value)
+    }
+    else if (e.target.name == 'desc') {
+      setDesc(e.target.value)
+    }
+
+  }
+
+
   return (
-    <div>
-      This is my Contact page
-    </div>
+
+    <>
+      <Head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous"></link>
+      </Head>
+
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous" />
+
+      <div className='container'>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">Name</label>
+            <input type="text" value={name} onChange={handleChange} className="form-control" id="name" name='name' /></div>
+
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" value={email} onChange={handleChange} className="form-control" id="email" name='email' aria-describedby="emailHelp" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="phone" className="form-label">Phone</label>
+            <input type="phone" value={phone} onChange={handleChange} className="form-control" id="phone" name='phone' />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="desc" className="form-label">Elaborate your concern</label>
+            <textarea className="form-control" value={desc} onChange={handleChange} placeholder="Write your concern here" id="desc" name='desc' ></textarea>
+
+          </div>
+
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </>
   )
 }
 
